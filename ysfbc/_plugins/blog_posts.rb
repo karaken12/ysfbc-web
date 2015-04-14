87aa61c8a0d8ae8b7ddb9f5287a13cb1c8782865
Tom Potts <tom@yorkscifibookclub.co.uk>
2015-04-14 14:02:33 +0100

    Start of process of generalising blog_posts plugin.

--- ysfbc/_plugins/blog_posts.rb
+++ ysfbc/_plugins/blog_posts.rb
@@ -3,23 +3,33 @@ require 'discourse_api'
 module Discourse
   class BlogEntries < Jekyll::Generator
      def generate(site)
+       # Set up config parameters
        maxtopics = site.config['max_blog_entries']
        if maxtopics.nil?
          maxtopics = -1
        end
 
+       category = 'blog'
+
+       # Set up Discourse client
        client = DiscourseApi::Client.new(site.config['discourse_base'])
        client.api_key = site.config['discourse_api_key']
        client.api_username = site.config['discourse_api_user']
-       topics = client.category_latest_topics('blog')
+
+       #TODO for each category:
+       topics = client.category_latest_topics(category)
        topics = topics.select{ |topic| topic['pinned'] == false }
+       # If set, restrict the number of topics returned
        if (maxtopics > 0 && topics.count > maxtopics)
          topics = topics[0..(maxtopics-1)]
        end
+       # Now retrieve the requested data.
        entries = []
        topics.each { |topic|
          entries.push(client.topic(topic['id']))
        }
+       #TODO for each category, put this away into a variable
+
        site.data['blog_entries'] = entries
      end
   end
