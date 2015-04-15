1d30cec750c89895370455b5f17a11e3a9b0d96d
Tom Potts <tom@yorkscifibookclub.co.uk>
2015-04-15 18:51:41 +0100

    Adding meetings.

--- ysfbc/_plugins/next_meeting.rb
+++ ysfbc/_plugins/next_meeting.rb
@@ -3,8 +3,7 @@ require 'discourse_api'
 module Discourse
   class NextMeeting < Jekyll::Generator
      def generate(site)
-       meeting = {'when'=> '7:00pm, Thursday 16th April 2015', 'where'=>'The Maltings Pub'}
-       meeting['facebook'] = {'event_id' => '742862479166342'}
+       meeting = site.data['meetings'][0]
        meeting['book'] = site.data['allbooks'][0]
        meeting['short'] = site.data['shorts'][0]
        site.data['next_meeting'] = meeting
