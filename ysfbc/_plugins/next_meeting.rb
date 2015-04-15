1e59849af1aa4b15849473409ed83509a274cbfe
Tom Potts <tom@yorkscifibookclub.co.uk>
2015-04-15 19:21:30 +0100

    Changed meeting data format.

--- ysfbc/_plugins/next_meeting.rb
+++ ysfbc/_plugins/next_meeting.rb
@@ -3,10 +3,19 @@ require 'discourse_api'
 module Discourse
   class NextMeeting < Jekyll::Generator
      def generate(site)
-       meeting = site.data['meetings'][0]
-       meeting['book'] = site.data['allbooks'][0]
-       meeting['short'] = site.data['shorts'][0]
+       name = 'April 2015'
+       meeting = site.data['meetings'][name]
        site.data['next_meeting'] = meeting
+
+       for book in site.data['allbooks']
+         meeting = site.data['meetings'][book['meeting_for']]
+         meeting['book'] = book
+       end
+
+       for short in site.data['shorts']
+         meeting = site.data['meetings'][short['meeting_for']]
+         meeting['short'] = short
+       end
      end
   end
 end
