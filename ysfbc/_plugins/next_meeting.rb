17dde9cf6e55b322cf9e51ad7eff92590ea16881
Tom Potts <tom@yorkscifibookclub.co.uk>
2015-04-19 20:39:57 +0100

    Changes to how meetings are displayed.

--- ysfbc/_plugins/next_meeting.rb
+++ ysfbc/_plugins/next_meeting.rb
@@ -3,18 +3,26 @@ require 'discourse_api'
 module Discourse
   class NextMeeting < Jekyll::Generator
      def generate(site)
-       name = 'April 2015'
+       name = 'May 2015'
        meeting = site.data['meetings'][name]
        site.data['next_meeting'] = meeting
 
        for book in site.data['allbooks']
-         meeting = site.data['meetings'][book['meeting_for']]
-         meeting['book'] = book
+         meeting_name = book['meeting_for']
+         if !site.data['meetings'].has_key?(meeting_name)
+           puts "Missing meeting: #{meeting_name}"
+           next
+         end
+         site.data['meetings'][meeting_name]['book'] = book
        end
 
        for short in site.data['shorts']
-         meeting = site.data['meetings'][short['meeting_for']]
-         meeting['short'] = short
+         meeting_name = short['meeting_for']
+         if !site.data['meetings'].has_key?(meeting_name)
+           puts "Missing meeting: #{meeting_name}"
+           next
+         end
+         site.data['meetings'][meeting_name]['short'] = short
        end
      end
   end
