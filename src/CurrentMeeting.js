300770c3d341d4db2cdff8427ed239e197bc6aab
Tom Potts <karaken12@gmail.com>
2019-12-21 18:59:26 +0000

    Fetch current meeting data from local URL

--- src/CurrentMeeting.js
+++ src/CurrentMeeting.js
@@ -1,20 +1,47 @@
 import React, {Component} from "react";
 import Meeting from "./Meeting";
 
-export class CurrentMeeting extends Component {
-  render() {
-    const meeting = {"date":"2019-12-19 19:00:00 +0000","where":"The Cross Keys","facebook":{"event_id":353283112176655},"name":"December 2019","book":{"title":"Lord of Light","author":"Roger Zelazny","meeting_for":"December 2019","slug":"lord-of-light","image-source":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1465576796l/8493781._SY475_.jpg","info-links":[{"name":"ISFDB","class":"isfdb","url":"http://www.isfdb.org/cgi-bin/title.cgi?1611"},{"name":"GoodReads","class":"goodreads","url":"https://www.goodreads.com/book/show/13821.Lord_of_Light"}],"store-links":[{"name":"Amazon paperback","class":"amazon","url":"https://www.amazon.co.uk/Lord-Light-MASTERWORKS-Roger-Zelazny/dp/0575094214"},{"name":"Amazon Kindle","class":"kindle","url":"https://www.amazon.co.uk/Lord-Light-Roger-Zelazny-ebook/dp/B07MSJZDFX"}],"ptype":"books","img-url":"/images/books/2019/lord-of-light.jpg"},"short":{"title":"Christmas Wedding","author":"Vylar Kaftan","meeting_for":"December 2019","slug":"christmas-wedding","image-source":"https://vylarkaftan.files.wordpress.com/2009/04/warrior_wisewoman.jpg","info-links":[{"name":"ISFDB","class":"isfdb","url":"http://www.isfdb.org/cgi-bin/title.cgi?1151999"},{"name":"GoodReads","class":"goodreads","url":"https://www.goodreads.com/book/show/19092067-christmas-wedding"}],"store-links":[{"name":"Escape Pod (free)","class":"escapepod free","url":"http://escapepod.org/2010/12/23/ep272-christmas-wedding"},{"name":"Giganotosaurus","class":"web free","url":"http://giganotosaurus.org/2013/12/01/christmas-wedding/"}],"ptype":"shorts","img-url":"/images/shorts/2019/christmas-wedding.jpg"},"film":{"title":"The Castle of Cagliostro","meeting_for":"December 2019","slug":"castle-of-cagliostro","image-source":"https://images.justwatch.com/poster/58748733/s592","info-links":[{"name":"IMDB","class":"imdb","url":"https://www.imdb.com/title/tt0079833"},{"name":"JustWatch","class":"justwatch","url":"https://www.justwatch.com/uk/movie/lupin-the-third-the-castle-of-cagliostro"}],"store-links":[{"name":"Netfix","class":"netflix","url":"https://www.netflix.com/title/0050576"}],"ptype":"films","img-url":"/images/films/2019/castle-of-cagliostro.jpg"}};
+const NEXT_MEETING_SERVICE_URL = '/data/next_meeting.json';
 
+export class CurrentMeeting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: false,
+      meeting: {}
+    };
+  }
+
+  render() {
     return <Meeting
-      key={meeting.name}
-      name={meeting.name}
-      where={meeting.where}
-      facebook={meeting.facebook}
-      date={meeting.date}
-      book={meeting.book}
-      short={meeting.short}
-      film={meeting.film}
+      key={this.state.meeting.name}
+      name={this.state.meeting.name}
+      where={this.state.meeting.where}
+      facebook={this.state.meeting.facebook}
+      date={this.state.meeting.date}
+      book={this.state.meeting.book}
+      short={this.state.meeting.short}
+      film={this.state.meeting.film}
       isCurrent={true}
     />;
   }
+
+  componentDidMount() {
+    this.fetchCurrentMeeting();
+  }
+
+  fetchCurrentMeetingWithFetchAPI = () => {
+    this.setState({...this.state, isFetching: true});
+    fetch(NEXT_MEETING_SERVICE_URL)
+      .then(response => response.json())
+      .then(result => {
+        this.setState({meeting: result, isFetching: false})
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({...this.state, isFetching: false});
+      });
+  };
+
+  fetchCurrentMeeting = this.fetchCurrentMeetingWithFetchAPI;
 }
