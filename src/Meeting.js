8e5d504d91dbf1334022da290474dffd6abdd86c
Tom Potts <karaken12@gmail.com>
2019-12-21 22:41:33 +0000

    Hack to use the existing images for the moment
    This means we can continue working on this without worrying about
    how the images will be uploaded.

--- src/Meeting.js
+++ src/Meeting.js
@@ -64,7 +64,8 @@ function Header(props) {
 
 function BookImage(props) {
     return <div className="bookimg">
-        <img src={props["img-url"]} alt={props.title}/>
+        {/*<img src={props["img-url"]} alt={props.title}/>*/}
+        <img src={"https://s3-eu-west-1.amazonaws.com/www.yorkscifibookclub.co.uk" + props["img-url"]} alt={props.title}/>
     </div>;
 }
 
