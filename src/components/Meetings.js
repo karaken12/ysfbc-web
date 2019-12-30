f2ba803c450da320580330424b6cbc112514a4b1
Tom Potts <karaken12@gmail.com>
2019-12-30 00:09:48 +0000

    Harmonise display of titles for meetings and books
    The headings for previous books, shorts and films were inadvertently
    ommitted in an earlier commit; add them back in, and move the heading
    for meetings into the loader for consistency.

--- src/components/Meetings.js
+++ src/components/Meetings.js
@@ -5,7 +5,6 @@ export function Meetings(props) {
   const meetings = props.meetings;
 
   return <>
-    <h1 className="books">Meetings</h1>
     {Object.entries(meetings).map((entry) => {
       var meeting = entry[1];
       return <Meeting meeting={meeting} key={meeting.name}/>;
--- src/components/loaders/MeetingsLoader.js
+++ src/components/loaders/MeetingsLoader.js
@@ -30,10 +30,22 @@ const MeetingsLoader = () => {
     return <LoadingSpinner/>
   } else {
     return <Switch>
-      <Route path={"/meetings"}><Meetings meetings={meetings}/></Route>
-      <Route path={"/books"}><Books meetings={meetings} type="book"/></Route>
-      <Route path={"/shorts"}><Books meetings={meetings} type="short"/></Route>
-      <Route path={"/films"}><Books meetings={meetings} type="film"/></Route>
+      <Route path={"/meetings"}>
+        <h1 className="books">Meetings</h1>
+        <Meetings meetings={meetings}/>
+      </Route>
+      <Route path={"/books"}>
+        <h1 className="books">Books</h1>
+        <Books meetings={meetings} type="book"/>
+      </Route>
+      <Route path={"/shorts"}>
+        <h1 className="books">Shorts</h1>
+        <Books meetings={meetings} type="short"/>
+      </Route>
+      <Route path={"/films"}>
+        <h1 className="books">Films</h1>
+        <Books meetings={meetings} type="film"/>
+      </Route>
     </Switch>;
   }
 };
