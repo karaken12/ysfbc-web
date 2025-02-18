7225e49b30aa71b6e2645a03c1c6de96db56ebd3
Kara Potts <karaken12@gmail.com>
2025-02-18 20:52:22 +0000

    Add films and shorts to current meetings

--- src/components/currentMeeting.tsx
+++ src/components/currentMeeting.tsx
@@ -8,6 +8,94 @@ import moment from "moment/moment";
 const constructImageUrl = (type: string, year: number, slug: string, extension: string = 'jpg') =>
   `/images/${type}/${year}/${slug}.${extension}`;
 
+type BookType = {
+  "info-links": Array<Queries.ContentfulInfoLink>;
+  "store-links": any;
+  meeting: MeetingType;
+  meeting_for: string;
+  title: string;
+  author: string;
+  "img-url": string;
+  image?: React.ReactElement;
+};
+
+type ShortType = {
+  "info-links": Array<Queries.ContentfulInfoLink>;
+  "store-links": any;
+  meeting: MeetingType;
+  meeting_for: string;
+  title: string;
+  author: string;
+  "img-url": string;
+  image?: React.ReactElement;
+};
+
+type FilmType = {
+  "info-links": Array<Queries.ContentfulInfoLink>;
+  "store-links": any;
+  meeting: MeetingType;
+  meeting_for: string;
+  title: string;
+  "img-url": string;
+  image?: React.ReactElement;
+};
+
+type MeetingType = {
+  name: string | null;
+  date: string | null;
+  where: any;
+  book?: BookType;
+  short?: ShortType;
+  film?: FilmType
+};
+
+const getBook = (book: Queries.ContentfulBook, currentMeeting: MeetingType): BookType => {
+  const year = moment.utc(currentMeeting.date).year()
+  const imageData = getImage(book.image);
+  const image = imageData ? <GatsbyImage image={imageData} alt={book.title ?? ''}/> : undefined;
+  return {
+    "info-links": book.infoLinks,
+    "store-links": book.storeLinks,
+    meeting: currentMeeting,
+    "meeting_for": currentMeeting.name,
+    title: book.title,
+    author: book.author,
+    "img-url": constructImageUrl("books", year, book.slug),
+    image: image,
+  }
+};
+
+const getShort = (short: Queries.ContentfulShort, currentMeeting: MeetingType): BookType => {
+  const year = moment.utc(currentMeeting.date).year()
+  const imageData = getImage(short.image);
+  const image = imageData ? <GatsbyImage image={imageData} alt={short.title ?? ''}/> : undefined;
+  return {
+    "info-links": short.infoLinks,
+    "store-links": short.storeLinks,
+    meeting: currentMeeting,
+    "meeting_for": currentMeeting.name,
+    title: short.title,
+    author: short.author,
+    "img-url": constructImageUrl("books", year, short.slug),
+    image: image,
+  }
+};
+
+const getFilm = (film: Queries.ContentfulFilm, currentMeeting: MeetingType): BookType => {
+  const year = moment.utc(currentMeeting.date).year()
+  const imageData = getImage(film.image);
+  const image = imageData ? <GatsbyImage image={imageData} alt={film.title ?? ''}/> : undefined;
+  return {
+    "info-links": film.infoLinks,
+    "store-links": film.storeLinks,
+    meeting: currentMeeting,
+    "meeting_for": currentMeeting.name,
+    title: film.title,
+    "img-url": constructImageUrl("books", year, film.slug),
+    image: image,
+  }
+};
+
 const CurrentMeeting = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -31,6 +119,40 @@ const CurrentMeeting = () => {
               url
             }
             storeLinks {
+              name
+              class
+              url
+            }
+          }
+          short {
+            author
+            slug
+            title
+            image {
+              gatsbyImageData(width: 150)
+            }
+            infoLinks {
+              class
+              url
+            }
+            storeLinks {
+              name
+              class
+              url
+            }
+          }
+          film {
+            slug
+            title
+            image {
+              gatsbyImageData(width: 150)
+            }
+            infoLinks {
+              class
+              url
+            }
+            storeLinks {
+              name
               class
               url
             }
@@ -39,28 +161,15 @@ const CurrentMeeting = () => {
       }
     }
   `)
-  const currentMeeting = {
-    name: data.allContentfulMeeting.nodes[0].title,
-    date: data.allContentfulMeeting.nodes[0].date,
-    where: data.allContentfulMeeting.nodes[0].location.name,
-    book: null,
-    short: null,
-    film: null,
+  const meeting = data.allContentfulMeeting.nodes[0];
+  const currentMeeting: MeetingType = {
+    name: meeting.title,
+    date: meeting.date,
+    where: meeting.location.name,
   }
-  const year = moment.utc(currentMeeting.date).year()
-  const book = data.allContentfulMeeting.nodes[0].book;
-  const imageData = getImage(book.image);
-  const bookImage = imageData ? <GatsbyImage image={imageData} alt={book.title}/> : undefined;
-  currentMeeting.book = {
-    "info-links": book.infoLinks,
-    "store-links": book.storeLinks,
-    meeting: currentMeeting,
-    "meeting_for": currentMeeting.name,
-    title: book.title,
-    author: book.author,
-    "img-url": constructImageUrl("books", year, book.slug),
-    image: bookImage,
-  }
+  currentMeeting.book = meeting.book ? getBook(meeting.book, currentMeeting) : undefined
+  currentMeeting.short = meeting.short ? getShort(meeting.short, currentMeeting) : undefined
+  currentMeeting.film = meeting.film ? getFilm(meeting.film, currentMeeting) : undefined
   return <Meeting meeting={currentMeeting} isCurrent={true} />
 }
 
