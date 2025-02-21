03f610e8be30d4bca278bf215975847e4f09e664
Kara Potts <karaken12@gmail.com>
2025-02-21 11:02:10 +0000

    Add Contentful information to meetings page
    Show side-by-side with existing meeting data for development purposes.
    Also make sure to only show the meetings for which we already have data,
    i.e. January 2024 and prior.

--- src/pages/meetings.tsx
+++ src/pages/meetings.tsx
@@ -1,15 +1,36 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import {graphql, HeadFC, PageProps} from "gatsby"
 import '../style.scss'
 // @ts-ignore
 import App from '../App.js'
 // @ts-ignore
 import {MeetingsPageContent} from '../components/PageContent.js'
+// @ts-ignore
+import {Meetings} from '../components/Meetings.js'
+import {translateContentfulMeeting} from "../data/translation/translateContentfulMeeting";
 
-const MeetingsPage: React.FC<PageProps> = () => {
+const MeetingsPage: React.FC<PageProps> = ({data}) => {
+  const meetings = data.allContentfulMeeting.nodes.map((meeting) => translateContentfulMeeting(meeting))
+
+  const columns = {
+    display: 'flex',
+  }
+
+  const columnStyle = {
+    width: "50%",
+  }
+
   return (
     <App>
-      <MeetingsPageContent />
+      <div style={columns}>
+        <div style={columnStyle}>
+          <MeetingsPageContent />
+        </div>
+        <div style={columnStyle}>
+          <h1 className="books">Meetings</h1>
+          <Meetings meetings={meetings}/>
+        </div>
+      </div>
     </App>
   )
 }
@@ -17,3 +38,71 @@ const MeetingsPage: React.FC<PageProps> = () => {
 export default MeetingsPage
 
 export const Head: HeadFC = () => <title>York SciFi Book Club</title>
+
+export const query = graphql`
+  query {
+    allContentfulMeeting(
+      filter: {date: {lt: "2024-02-01T00:00+00:00"}}
+      sort: {date: DESC}
+    ) {
+      nodes {
+        contentful_id
+        title
+        date
+        location {
+          name
+        }
+        book {
+          author
+          slug
+          title
+          image {
+            gatsbyImageData(width: 150)
+          }
+          infoLinks {
+            class
+            url
+          }
+          storeLinks {
+            name
+            class
+            url
+          }
+        }
+        short {
+          author
+          slug
+          title
+          image {
+            gatsbyImageData(width: 150)
+          }
+          infoLinks {
+            class
+            url
+          }
+          storeLinks {
+            name
+            class
+            url
+          }
+        }
+        film {
+          slug
+          title
+          image {
+            gatsbyImageData(width: 150)
+          }
+          infoLinks {
+            class
+            url
+          }
+          storeLinks {
+            name
+            class
+            url
+          }
+        }
+      }
+    }
+  }
+`
