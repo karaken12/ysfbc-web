46a963e4faf90fa585e446db19ca6fd4288037d8
Kara Potts <karaken12@gmail.com>
2025-02-25 11:10:11 +0000

    Add info links to book entries

--- scripts/data-import.ts
+++ scripts/data-import.ts
@@ -202,12 +202,83 @@ const main = async () => {
     slug: {"en-GB": bookData.slug},
   });
 
+  const updateInfoLinks = async (bookEntry, bookData: SourceBookData) => {
+    const existingLinks = (bookEntry.fields.infoLinks?.['en-GB'] ?? []).map(entry => entry.sys.id)
+    const linkEntries = []
+    for (const linkId of existingLinks) {
+      const entry = await client.entry.get({entryId: linkId})
+      linkEntries.push(entry)
+    }
+    const infoLinkData = bookData["info-links"] ?? [];
+
+    if (deepEqual(
+      linkEntries.map(entry => [entry.fields.class['en-GB'], entry.fields.url['en-GB']]),
+      infoLinkData.map(entry => [entry.class, entry.url])
+    )) {
+      return bookEntry
+    }
+
+    console.log(`Updating info links for ${bookData.title}`);
+
+    if (linkEntries.length == 0) {
+      // create new entries
+      const infoLinkRefs = []
+      for (const data of infoLinkData) {
+        console.log(`Creating ${data.class} info link`)
+        const newInfoLinkEntry = await client.entry.create({contentTypeId: 'infoLink'}, {
+          fields: {
+            "class": {"en-GB": data.class},
+            "url": {"en-GB": data.url},
+          }
+        })
+        infoLinkRefs.push(
+          {sys: {type: 'Link', linkType: 'Entry', id: newInfoLinkEntry.sys.id}}
+        )
+      }
+      // add entries to bookEntry
+      return await client.entry.update({entryId: bookEntry.sys.id},
+        {
+          ...(bookEntry),
+          fields: {
+            ...bookEntry.fields,
+            infoLinks: {"en-GB": infoLinkRefs},
+          }
+        }
+      )
+    } else if (linkEntries.length == infoLinkData.length) {
+      // update existing entries
+      const zipped = linkEntries.map((entry, i) => ({entry, data: infoLinkData[i]}))
+      for (const {entry, data} of zipped) {
+        const fields = {
+          "class": {"en-GB": data.class},
+          "url": {"en-GB": data.url},
+        };
+        if (deepEqual(entry.fields, fields)) {
+          continue;
+        }
+        console.log(`Updating ${data.class} info link`)
+        await client.entry.update(
+          {entryId: entry.sys.id},
+          {
+            ...entry,
+            fields,
+          }
+        );
+      }
+      return bookEntry;
+    } else {
+      throw new Error(`Info links for ${bookData.title} require manual intervention (${infoLinkData.length} expected; ${linkEntries.length} found)`)
+    }
+  };
+
   const createOrUpdateBook = async (meetingEntry, bookData: SourceBookData) => {
     if (meetingEntry.fields.book) {
       const bookEntryId = meetingEntry.fields.book['en-GB'].sys.id;
-      return await updateBookEntry(bookEntryId, bookData)
+      const bookEntry = await updateBookEntry(bookEntryId, bookData);
+      return await updateInfoLinks(bookEntry, bookData)
     } else {
-      return await createBookEntry(bookData)
+      const bookEntry = await createBookEntry(bookData);
+      return await updateInfoLinks(bookEntry, bookData)
     }
   };
 
