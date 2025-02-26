1aff6a67eab703ee8420d049548e5a5fe02d58c0
Kara Potts <karaken12@gmail.com>
2025-02-26 19:32:30 +0000

    Fix type errors

--- scripts/data-import.ts
+++ scripts/data-import.ts
@@ -1,15 +1,19 @@
-import {createClient} from 'contentful-management'
+import {createClient, EntryProps} from 'contentful-management'
 import dotenv from 'dotenv'
 import moment from "moment/moment";
 import {getSourceMeetings, SourceBookData} from "./data-import/get-source-data";
 import {locationFunctions} from "./data-import/locationFunctions";
-import {MeetingData, meetingFunctions} from "./data-import/meeting-functions";
+import {ContentfulMeeting, MeetingData, meetingFunctions} from "./data-import/meeting-functions";
+import {ContentfulEntryLink, createEntryLink, deepEqual, localise, Localised} from "./data-import/utils";
 
 dotenv.config()
 
-const deepEqual = (lhs: object, rhs: object) => {
-  // A hack to check for deep equality
-  return JSON.stringify(lhs) === JSON.stringify(rhs);
+type ContentfulBook = {
+  title: Localised<string>;
+  author: Localised<string>;
+  slug: Localised<string>;
+  infoLinks?: Localised<Array<ContentfulEntryLink>>;
+  storeLinks?: Localised<object | object[] | undefined>;
 };
 
 const main = async () => {
@@ -34,18 +38,18 @@ const main = async () => {
   const {getContentfulLocation} = locationFunctions(client);
   const {createOrUpdateMeeting, updateMeetingWithBook} = meetingFunctions(client, getContentfulLocation);
 
-  const getBookFields = (bookData: SourceBookData) => {
+  const getBookFields = (bookData: SourceBookData): ContentfulBook => {
     const hasStoreLinks = bookData["store-links"] && bookData["store-links"].length > 0;
 
     return ({
-      title: {"en-GB": bookData.title},
-      author: {"en-GB": bookData.author},
-      slug: {"en-GB": bookData.slug},
-      ...(hasStoreLinks ? {storeLinks: {"en-GB": bookData["store-links"]}} : {}),
+      title: localise(bookData.title),
+      author: localise(bookData.author),
+      slug: localise(bookData.slug),
+      ...(hasStoreLinks ? {storeLinks: localise(bookData["store-links"])} : {}),
     });
   };
 
-  const updateInfoLinks = async (bookEntry, bookData: SourceBookData) => {
+  const updateInfoLinks = async (bookEntry: EntryProps<ContentfulBook>, bookData: SourceBookData): Promise<EntryProps<ContentfulBook>> => {
     const existingLinks = (bookEntry.fields.infoLinks?.['en-GB'] ?? []).map(entry => entry.sys.id)
     const linkEntries = []
     for (const linkId of existingLinks) {
@@ -70,21 +74,19 @@ const main = async () => {
         console.log(`Creating ${data.class} info link`)
         const newInfoLinkEntry = await client.entry.create({contentTypeId: 'infoLink'}, {
           fields: {
-            "class": {"en-GB": data.class},
-            "url": {"en-GB": data.url},
+            "class": localise(data.class),
+            "url": localise(data.url),
           }
         })
-        infoLinkRefs.push(
-          {sys: {type: 'Link', linkType: 'Entry', id: newInfoLinkEntry.sys.id}}
-        )
+        infoLinkRefs.push(createEntryLink(newInfoLinkEntry.sys.id));
       }
       // add entries to bookEntry
-      return await client.entry.update({entryId: bookEntry.sys.id},
+      return await client.entry.update<ContentfulBook>({entryId: bookEntry.sys.id},
         {
           ...(bookEntry),
           fields: {
             ...bookEntry.fields,
-            infoLinks: {"en-GB": infoLinkRefs},
+            infoLinks: localise(infoLinkRefs),
           }
         }
       )
@@ -93,8 +95,8 @@ const main = async () => {
       const zipped = linkEntries.map((entry, i) => ({entry, data: infoLinkData[i]}))
       for (const {entry, data} of zipped) {
         const fields = {
-          "class": {"en-GB": data.class},
-          "url": {"en-GB": data.url},
+          "class": localise(data.class),
+          "url": localise(data.url),
         };
         if (deepEqual(entry.fields, fields)) {
           continue;
@@ -114,7 +116,7 @@ const main = async () => {
     }
   };
 
-  const createOrUpdateBook = async (meetingEntry, bookData: SourceBookData) => {
+  const createOrUpdateBook = async (meetingEntry: EntryProps<ContentfulMeeting>, bookData: SourceBookData): Promise<EntryProps<ContentfulBook>> => {
     if (meetingEntry.fields.book) {
       const bookEntryId = meetingEntry.fields.book['en-GB'].sys.id;
       const bookEntry = await updateBookEntry(bookEntryId, bookData);
@@ -125,10 +127,10 @@ const main = async () => {
     }
   };
 
-  const createBookEntry = async (bookData: SourceBookData) => {
+  const createBookEntry = async (bookData: SourceBookData): Promise<EntryProps<ContentfulBook>> => {
     const fields = getBookFields(bookData);
     console.log(`Creating book ${fields.title["en-GB"]}`)
-    return client.entry.create(
+    return client.entry.create<ContentfulBook>(
       {contentTypeId: 'book'},
       {
         fields,
@@ -136,8 +138,8 @@ const main = async () => {
     )
   };
 
-  const updateBookEntry = async (entryId: string, bookData: SourceBookData) => {
-    const entry = await client.entry.get({entryId})
+  const updateBookEntry = async (entryId: string, bookData: SourceBookData): Promise<EntryProps<ContentfulBook>> => {
+    const entry = await client.entry.get<ContentfulBook>({entryId})
     const fields = {
       ...entry.fields,
       ...(getBookFields(bookData)),
